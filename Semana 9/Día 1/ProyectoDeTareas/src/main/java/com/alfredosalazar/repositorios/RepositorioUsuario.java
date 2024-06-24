d36d61530584ad0d1e912c81d449f83a81e201e6
Alfredo08 <alfredosv08@gmail.com>
2024-06-24 16:33:09 -0600

    Full CRUD

---
+++ Semana 9/Día 1/ProyectoDeTareas/src/main/java/com/alfredosalazar/repositorios/RepositorioUsuario.java
@@ -0,0 +1,17 @@
+package com.alfredosalazar.repositorios;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alfredosalazar.modelos.Usuario;
+
+@Repository
+public interface RepositorioUsuario extends CrudRepository<Usuario, Long> {
+	/*
+	 * SELECT *
+	 * FROM usuarios;
+	 */
+	List<Usuario> findAll();
+}
